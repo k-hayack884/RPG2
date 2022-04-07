dceba96df816b1ddd1a36b3a60919dac50c51ee7
k-hayack884 <hayack885@gmail.com>
2022-04-07 21:38:34 +0900

    戦闘終了

---
+++ irasutoya/hero.php
@@ -0,0 +1,22 @@
+<?php
+class Hero{
+    const MAX_HP=100;
+    public $name;
+    public $hitpoint=100;
+    public $attack=20;
+
+    public function doAttack($enemy)
+    {
+        echo "「". $this->name."の攻撃」".PHP_EOL;
+        echo "「". $enemy->name."に".$this->attack."のダメージを与えた！」".PHP_EOL;
+        $enemy->tookDamage($this->attack);
+
+    }
+    public function tookDamage($damage)
+    {
+        $this->hitpoint-=$damage;
+        if($this->hitpoint<0){
+            $this->hitpoint=0;
+        }
+    }
+}
---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+require_once('./irasutoya/Hero.php');
+require_once('./irasutoya/Enemy.php');
+
+echo 'やったぜ'.PHP_EOL;
+
+$man=new Hero();
+$odoru_hito=new Enemy();
+
+$man->name="男性";
+$odoru_hito->name="踊る女性";
+
+$turn=1;
+while ($man->hitpoint>0 && $odoru_hito->hitpoint>0){
+    echo $turn."ターン開始！".PHP_EOL;
+echo $man->name."のHPは".$man->hitpoint."/".$man::MAX_HP."です".PHP_EOL;
+echo $odoru_hito->name."のHPは".$odoru_hito->hitpoint."/".$odoru_hito::MAX_HP."です".PHP_EOL;
+
+$man->doAttack($odoru_hito);
+$odoru_hito->doAttack($man);
+
+$turn++;
+echo PHP_EOL;
+}
+echo "戦闘終了！".PHP_EOL;
